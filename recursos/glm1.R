8c2d393ba8565ba8716847f90ea5eab57c6bf040
EHPEI <juan.molina1.ext@bayer.com>
2022-06-17 01:16:07 -0300

    repo final

--- recursos/glm1.R
+++
@@ -1,6 +0,0 @@
-library(lme4) 
-library(RVAideMemoire)
-Data$obs <- factor(formatC(1:nrow(Data), flag="0", width = 3))
-model.glmm <- glmer(cbind(number_pres,number_abs) ~ Var1+Var2+Var3+Var4...+
-                      (1|obs),family = binomial (link = logit),data = Data) 
-overdisp.glmer(model.glmm) #Overdispersion for GLMM
